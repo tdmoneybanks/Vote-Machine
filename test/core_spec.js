b8d5624ffc792634ae2e44ab310f68f8aea95e48
tyler dunkel <tdunkel@iwu.edu>
2015-12-25 12:58:12 -0600

    voting function and tests

--- test/core_spec.js
+++ test/core_spec.js
@@ -2,7 +2,7 @@ import {List, Map} from 'immutable';
 import {expect} from 'chai';
 
 //app-specific imports
-import {setEntries} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
 describe('application-code', () => {
 
@@ -23,6 +23,68 @@ describe('application-code', () => {
 			const nextState = setEntries(state, entries);
 			expect(nextState).to.equal(Map({
 				entries: List.of('Trainspotting', '28 Days Later')
+			}));
+		});
+	});
+
+	describe('next', () => {
+
+		it('takes the next two entries under vote', () => {
+			const state = Map({
+				entries: List.of('Trainspotting', '28 Days Later', 'Sunshine')
+			});
+			const nextState = next(state);
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List.of('Sunshine')
+			}));
+		});
+	});
+
+	describe('vote', () => {
+
+		it('creates a tally for the voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later')
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 1
+					})
+				}),
+				entries: List()
+			}));
+		});
+
+		it('adds to existing tally for voted entry', () => {
+			const state = Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 2,
+						'28 Days Later': 3
+					})
+				}),
+				entries: List()
+			});
+			const nextState = vote(state, 'Trainspotting');
+			expect(nextState).to.equal(Map({
+				vote: Map({
+					pair: List.of('Trainspotting', '28 Days Later'),
+					tally: Map({
+						'Trainspotting': 3,
+						'28 Days Later': 3
+					})
+				}),
+				entries: List()
 			}));
 		});
 	});
